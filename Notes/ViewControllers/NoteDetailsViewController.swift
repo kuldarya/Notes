2873d60dc405c1c95a774c54dbc25a6d48e95754
Daria.Kuleshova <daria.kuleshova@life.com.by>
2019-12-04 19:14:35 +0300

    Note's textField enhancement

--- Notes/ViewControllers/NoteDetailsViewController.swift
+++ Notes/ViewControllers/NoteDetailsViewController.swift
@@ -110,6 +110,23 @@ extension NoteDetailsViewController: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         showDoneButton()
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == noteTitleTextField {
+            noteBodyTextView.becomeFirstResponder()
+        }
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let textFieldText = textField.text,
+            let rangeOfTextToReplace = Range(range, in: textFieldText) else {
+                return false
+        }
+        let substringToReplace = textFieldText[rangeOfTextToReplace]
+        let count = textFieldText.count - substringToReplace.count + string.count
+        return count <= 35
+    }
 }
 
 
